e70b92029fb061ca42f9c494187081eca60ec14f
dengx <dengxy2023@163.com>
2021-11-15 20:36:58 +0800

    20211115

--- qusetions/Q9_IsPalindrome.java
+++ qusetions/Q9_IsPalindrome.java
@@ -13,8 +13,21 @@ public class Q9_IsPalindrome {
         return flag;
     }
 
+    public boolean isPalindrome_2(int x){
+        if(x<0||(x%10==0&&x!=0)){
+            return false;
+        }
+        int reverteNum = 0;
+        while(x>reverteNum){
+            reverteNum = reverteNum*10+x%10;
+            x =x/10;
+        }
+        return x==reverteNum||x==reverteNum/10;
+    }
+
     public static void main(String[] args) {
         Q9_IsPalindrome q9 = new Q9_IsPalindrome();
-        System.out.println(q9.isPalindrome(-121));
+        System.out.println(q9.isPalindrome(121));
+        System.out.println(q9.isPalindrome_2(121));
     }
 }
