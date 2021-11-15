c9e3301bbbac28d22d2ab0c74460c44805af2f80
dengx <dengxy2023@163.com>
2021-11-15 21:37:22 +0800

    20211115

--- qusetions/Q6_Convert.java
+++ qusetions/Q6_Convert.java
@@ -1,2 +1,37 @@
-package qusetions;public class Q6_Convert {
+package qusetions;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q6_Convert {
+    public String convert(String s, int numRows){
+        if (numRows==1){
+            return s;
+        }
+        List<StringBuilder> rows = new ArrayList<>();
+        for(int i =0;i<numRows;i++){
+           rows.add(new StringBuilder());
+        }
+        int corRow =0;
+        boolean goingDown = false;
+        for(int i=0; i<s.length(); i++){
+            rows.get(corRow).append(s.charAt(i));
+            if(corRow==0||corRow==numRows-1){
+                goingDown = !goingDown;
+            }
+            corRow += goingDown ? 1:-1;
+        }
+        StringBuilder sNew = new StringBuilder();
+        for(int j=0; j<numRows; j++){
+            sNew.append(rows.get(j));
+        }
+
+        return sNew.toString();
+    }
+
+    public static void main(String[] args) {
+        Q6_Convert q6 = new Q6_Convert();
+        System.out.println(q6.convert("PAYPALISHIRING",4));
+    }
+
+
 }
