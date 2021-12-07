c8c9151f8feddce71a23c12516fcaa3dd65ed25b
dengx <dengxy2023@163.com>
2021-12-07 22:37:44 +0800

    2021/12/07

--- qusetions/Q658_FindClosestElements.java
+++ qusetions/Q658_FindClosestElements.java
@@ -1,17 +1,27 @@
 package qusetions;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class Q658_FindClosestElements {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b)->{
+            if(a[1]-b[1]==0){
+                return a[0]-b[0];
+            }
+            return a[1]-b[1];
+        });
+        List<Integer> res = new ArrayList<>();
         int[][] arrDis = new int[arr.length][2];
         for(int i=0;i<arr.length;i++){
-
+            queue.offer(new int[]{arr[i], Math.abs(arr[i]-x)});
         }
-        queue.offer(new int[]{arr[0], Math.abs(arr[0]-x)});
-        while(k>0)
-
+        while(k>0){
+            res.add(queue.poll()[0]);
+            k--;
+        }
+        res.sort((a,b)->a-b);
+        return res;
     }
 }
