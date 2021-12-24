be3684386769d4959d13db6bb45f1f61b32007d2
dengx <dengxy2023@163.com>
2021-12-24 10:39:28 +0800

    2021/12/24

--- qusetions/Q28_StrStr.java
+++ qusetions/Q28_StrStr.java
@@ -24,8 +24,42 @@ public class Q28_StrStr {
         }
         return -1;
     }
+    // 字符串滚动哈希，RK算法
+    public int strStr_RK(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        if (m == 0) {
+            return 0;
+        }
+        if(n<m){
+            return -1;
+        }
+        int k1 = 41;
+        int k2 = 1000000009;
+        // 计算needle哈希值
+        long hashNeedle = 0;
+        long hashHaystack = 0, extra=1;
+        for(int i=0;i<m;i++){
+            int c_needle = needle.charAt(i);
+            hashNeedle = (hashNeedle*k1+c_needle)%k2;
+        }
+        for(int i=0;i<m-1;i++){
+            int c_haystack = haystack.charAt(i);
+            hashHaystack = (hashHaystack*k1+c_haystack)%k2;
+            extra = extra*k1%k2;
+        }
+        for(int i=0;i<n-m+1;i++){
+            hashHaystack = (hashHaystack*k1+haystack.charAt(i+m-1))%k2;
+            if(hashHaystack==hashNeedle){
+                return i;
+            }
+            hashHaystack = (hashHaystack-haystack.charAt(i)*extra)%k2;
+            hashHaystack = (hashHaystack+k2)%k2;
+
+        }
+        return -1;
+    }
     // KMP
-    public int strStr_2(String haystack, String needle) {
+    public int strStr_KMP(String haystack, String needle) {
         int n = haystack.length(), m = needle.length();
         if (m == 0) {
             return 0;
@@ -56,9 +90,9 @@ public class Q28_StrStr {
 
     public static void main(String[] args) {
         Q28_StrStr q28 = new Q28_StrStr();
-        String str1 = "cdaaaabaabbaabaaab";
-        String str2 = "aabaaab";
-        int a = q28.strStr_2(str1, str2);
+        String str1 = "mississippi";
+        String str2 = "issi";
+        int a = q28.strStr_RK(str1, str2);
         System.out.println(a);
     }
 }
